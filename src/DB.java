f6c5a4ed7e53d09b057ffb3af430d213e535f2ae
Artem Petrikov <manforten@tandex.ru>
2023-02-11 21:00:00 +0300

    New changes

--- src/DB.java
+++ src/DB.java
@@ -4,13 +4,23 @@ public class DB {
     public static Connection conn;
     public static Statement stmt;
 
-    public static void connectOrCreateDB(String path) throws SQLException, ClassNotFoundException {//создание или подключение БД
+    public static void connectOrCreateDB (String path) throws SQLException, ClassNotFoundException {
         Class.forName("org.sqlite.JDBC");
-        conn = DriverManager.getConnection(path);//создаем соединение
-        stmt = conn.createStatement();//получаем доступ к БД для выполнения SQL запросов
+        conn = DriverManager.getConnection(path);
+        stmt = conn.createStatement();
     }
 
-    public static void createTable() throws SQLException{//создание таблицы
-        stmt.execute("CREATE TABLE if not exists 'types' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'type' VARCHAR(100) NOT NULL);");
+    public static void createTable() throws SQLException{ // Создание таблицы
+        stmt.executeUpdate("CREATE TABLE if not exists 'types' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'type' VARCHAR(100) UNIQUE NOT NULL)");
+    }
+
+    public static void insertType (String type) // Добавление типа кошки
+    {
+        try {
+            stmt.executeUpdate("INSERT INTO 'types' ('type') VALUES ('" + type + "')");
+        }
+        catch (SQLException s) {
+            System.out.println("Запись '"+type+"' уже существует");
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         DB.connectOrCreateDB("jdbc:sqlite:My_cats.db");
         DB.createTable();
+        DB.insertType("Абиссинская кошка");
+        DB.insertType("Австралийский мист");
+        DB.insertType("Американская жесткошерстная");
     }
 }
